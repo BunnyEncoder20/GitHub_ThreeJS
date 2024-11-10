69ed8e68849d3df4913d9d44aaab2e3a06dac98b
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2024-11-10 14:21:53 +0530

    enough practice

--- ReactThree/src/Components/RotatingCube.jsx
+++ ReactThree/src/Components/RotatingCube.jsx
@@ -1,5 +1,6 @@
 import { useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
+import { Sparkles } from '@react-three/drei';
 
 const RotatingCube = () => {
     const meshRef = useRef()
@@ -15,6 +16,7 @@ const RotatingCube = () => {
         <mesh ref={meshRef}>
             <torusGeometry args={[5,1,8,50]} />
             <meshPhysicalMaterial color="#468585" emissive="#468585" roughness={1} metalness={1} reflection={1} />
+            <Sparkles count={100} scale={6} size={5} speed={0.02} color="yellow" />
         </mesh>
     )
 }
